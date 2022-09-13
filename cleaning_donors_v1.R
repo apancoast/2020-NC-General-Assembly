7e4ca92af1560c9e83f0ee631a4a53a26ec678b5
apancoast <98405259+apancoast@users.noreply.github.com>
2022-09-13 16:45:36 -0400

    *Now* ready to fix entity donor names.

--- cleaning_donors_v1.R
+++ cleaning_donors_v1.R
@@ -2,8 +2,8 @@ library(tidyverse)
 
 # Exploring Donors ----
 #let's get that table real purrrty
-nc_ga_2020_elected <- read.csv("~/RStudio/state_congress/CSVs/nc_ga_2020_elected.csv")
-donors <- read.csv("~/RStudio/state_congress/CSVs/donors.csv") %>%
+nc_ga_2020_elected <- read.csv("D:/RStudio/state_congress/CSVs/nc_ga_2020_elected.csv")
+donors <- read.csv("D:/RStudio/state_congress/CSVs/donors.csv") %>%
   select( -matches("X")) %>%
   rename(tot_to_candidate = donations_total,
          tot_from_donor = Total.Donations..Click.to.sort.Ascending.,
@@ -24,16 +24,17 @@ entity_donors <- donors %>%
   arrange(donor_name)
 
 entity_donors %>%
-  mutate(across(
-    everything(),
-    gsub,
-    c("10th Cong Dist Gop|10th Cong Dist Republican Party|10th District Republican Party|10th District Repulican Party"),
-    "10th Congressional District Republican Party")) %>%
-  mutate(across(
-    everything(),
-    gsub,
-    c(" "),
-    " ")) %>%
+  mutate(donor_name=
+           str_replace_all(
+             donor_name,
+             "10th District Repulican Party|10th Cong Dist Gop|10th Cong Dist Republican Party|10th District Republican Party|10th District Repulican Party",
+             "10th Congressional District Republican Party"),
+         donor_name=
+           str_replace_all(
+             donor_name,
+             "3rd District Republican Party|3rd Cong District Republican",
+             "3rd Congressional District Republican Party")
+         ) %>%
   View()
 
 
