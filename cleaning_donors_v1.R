e484a015ca9be40af9b25d147d374265c59bd493
apancoast <98405259+apancoast@users.noreply.github.com>
2022-09-16 08:26:53 -0400

    #369

--- cleaning_donors_v1.R
+++ cleaning_donors_v1.R
@@ -28,274 +28,88 @@ entity_donors %>%
   distinct(donor_name) %>%
   count()
 #result 1667
-row_replace <- function(rows, name) {
+replace <- function(rows, name) {
   slice <- entity_donors %>%
     slice({{rows}}) %>%
     mutate(donor_name={{name}})
 
-  entity_donors %>%
+   entity_donors %>%
     rows_update(slice, by = c("id"))
+
 }
 
-entity_donors <- row_replace(1:6, "10th Congressional District Republican Party")
-entity_donors <- row_replace(8:12, "3M Co PAC")
+entity_donors <- entity_donors %>%
+  mutate(donor_name=
+         str_replace_all(
+           donor_name,
+           "Gop",
+           "GOP"),
+         donor_name=
+         str_replace_all(
+           donor_name,
+           "Atmc",
+           "ATMC"),
+         donor_name=
+         str_replace_all(
+           donor_name,
+           "Asd",
+           "ASD")
+  )
 
 
 
 # Row Updates ----
-#gotta do a lil fix for rows 176:193
-third <- entity_donors %>%
-  slice(13:14) %>%
-  mutate(donor_name="3rd Congressional District Republican Party")
+entity_donors <- replace(1:6, "10th Congressional District Republican Party")
+entity_donors <- replace(8:12, "3M Co PAC")
+entity_donors <- replace(13:14, "3rd Congressional District Republican Party")
+entity_donors <- replace(c(15,176:193), "Apartment Association of North Carolina")
+entity_donors <- replace(c(16:27,105:107), "American Council of Engineering Companies")
+entity_donors <- replace(c(28:29,312:313), "Auto Care Association's PAC")
+entity_donors <- replace(30:31, "ActBlue")
+entity_donors <- replace(32, "ADP as Depository for Taxes")
+entity_donors <- replace(34:43, "Ahold Delhaize USA, Inc PAC")
+entity_donors <- replace(45:46, "Alamance County of Republican Party Building Fund")
+entity_donors <- replace(47:48, "Alamance Republican Women")
+entity_donors <- replace(49, "Alexander County Democratic Executive Committee")
+entity_donors <- replace(50, "Alexander County Republican Party")
+entity_donors <- replace(52:53, "Alleghany County Republican Party")
+entity_donors <- replace(61:71, "Allstate Insurance Comp PAC")
+entity_donors <- replace(75:78, "Amazon")
+entity_donors <- replace(79:97, "American Airlines Inc PAC")
+entity_donors <- replace(98:104, "American Anesthesiology of NC PAC")
+entity_donors <- replace(108:116, "American Federation of State County and Municipal Employees (AFSCME-PEOPLE)")
+entity_donors <- replace(117:124, "American Kennel Club PAC")
+entity_donors <- replace(c(199:201,4099:4104), "American Physical Therapy Association NC PAC")
+entity_donors <- replace(c(129:141, 2111), "American Property Casualty Insurance Association PAC")
+entity_donors <- replace(144:157, "Anesthesiologists of the Triad PAC")
+entity_donors <- replace(158:159, "Anson County Republican Party")
+entity_donors <- replace(160:175, "Anthem Inc PAC")
+entity_donors <- replace(203, "Aristotle PAC")
+entity_donors <- replace(209:229, "Astellas US LLC PAC")
+entity_donors <- replace(c(230:278,310), "AT&T NC PAC")
+entity_donors <- replace(c(314, 3117:3211, 4635:4639), "NC Auto Dealers Accociation PAC")
+entity_donors <- replace(315:317, "IATSE 491 Back to One PAC")
+entity_donors <- replace(318:325, "Bank of America Corp State and Federal PAC")
+entity_donors <- replace(328:341, "Bayada Home Health Care Inc NC PAC")
+entity_donors <- replace(342:354, "Bayer US LLC PAC")
+entity_donors <- replace(355:360, "BB&T Corp")
+entity_donors <- replace(c(361:363,382:444,5566:5567), "Blue Cross & Blue Shield of NC Employees PAC")
+entity_donors <- replace(364:365, "Beasley Media Group")
 
-entity_donors <- entity_donors %>%
-  rows_update(third, by = c("id"))
-
-aanc <-
-  entity_donors %>%
-  slice(15, 176:193) %>%
-  mutate(donor_name="Apartment Association of North Carolina")
-
-entity_donors <- entity_donors %>%
-  rows_update(aanc, by = c("id"))
-
-
-donor_name=
-  str_replace_all(
-    donor_name,
-    "Acec NC PAC|Acec of NC PAC|Acec of North Carolina PAC",
-    "American Council of Engineering Companies")
-
-donor_name=
-  str_replace_all(
-    donor_name,
-    " \\(Acec/NC\\) PAC| \\(Acec/PAC\\)",
-    "")
-
-
-
-
-
-
-
-
-
-ahold <-
-  entity_donors %>%
-  slice(34:43) %>%
-  mutate(donor_name="Ahold Delhaize USA, Inc PAC")
-
-entity_donors <- entity_donors %>%
-  rows_update(ahold, by = c("id"))
-
-allstate <-
-  entity_donors %>%
-  slice(61:71) %>%
-  mutate(donor_name="Allstate Insurance Comp PAC")
-
-entity_donors <- entity_donors %>%
-  rows_update(allstate, by = c("id"))
-
-amazon <-
-  entity_donors %>%
-  slice(75:78) %>%
-  mutate(donor_name="Amazon")
-
-entity_donors <- entity_donors %>%
-  rows_update(amazon, by = c("id"))
-
-aapac <-
-  entity_donors %>%
-  slice(79:97) %>%
-  mutate(donor_name="American Airlines Inc PAC")
-
-entity_donors <- entity_donors %>%
-  rows_update(aapac, by = c("id"))
-
-akc <-
-  entity_donors %>%
-  slice(117:124) %>%
-  mutate(donor_name="American Kennel Club PAC")
-
-entity_donors <- entity_donors %>%
-  rows_update(akc, by = c("id"))
-
-apta <-
-  entity_donors %>%
-  slice(199:201,4099:4104) %>%
-  mutate(donor_name="American Physical Therapy Association NC PAC")
-
-entity_donors <- entity_donors %>%
-  rows_update(apta, by = c("id"))
-
-apcia <-
-  entity_donors %>%
-  slice(129:141, 2111) %>%
-  mutate(donor_name="American Property Casualty Insurance Association PAC")
-
-entity_donors <- entity_donors %>%
-  rows_update(apcia, by = c("id"))
-
-anthem <-
-  entity_donors %>%
-  slice(160:175) %>%
-  mutate(donor_name="Anthem Inc PAC")
-
-entity_donors <- entity_donors %>%
-  rows_update(anthem, by = c("id"))
-
-astellas <-
-  entity_donors %>%
-  slice(209:229) %>%
-  mutate(donor_name="Astellas US LLC PAC")
-
-entity_donors <- entity_donors %>%
-  rows_update(astellas, by = c("id"))
-
-att <-
-  entity_donors %>%
-  slice(230:278,310) %>%
-  mutate(donor_name="AT&T NC PAC")
-
-entity_donors <- entity_donors %>%
-  rows_update(att, by = c("id"))
-
-iatse <-
-  entity_donors %>%
-  slice(315:317) %>%
-  mutate(donor_name="IATSE 491 Back to One PAC")
-
-entity_donors <- entity_donors %>%
-  rows_update(iatse, by = c("id"))
-
-BoA <-
-  entity_donors %>%
-  slice(318:325) %>%
-  mutate(donor_name="Bank of America Corp State and Federal PAC")
-
-entity_donors <- entity_donors %>%
-  rows_update(BoA, by = c("id"))
-
-bayada <-
-  entity_donors %>%
-  slice(328:341) %>%
-  mutate(donor_name="Bayada Home Health Care Inc NC PAC")
-
-entity_donors <- entity_donors %>%
-  rows_update(bayada, by = c("id"))
 
 entity_donors %>%
   distinct(donor_name) %>%
   count()
-#result 1617
+#result 1558
+
 entity_donors %>%
   distinct(donor_name) %>%
   View()
 
-
-# Row by row ----
-entity_donors <-
-  entity_donors %>%
-  mutate(donor_name=
-           str_replace_all(
-             donor_name,
-             "Acpac Automobile Club|Automobile Club PAC",
-             "Auto Care Association's PAC"),
-         donor_name=
-           str_replace_all(
-             donor_name,
-             "Actbluedonate|Act Blue",
-             "ActBlue"),
-         donor_name=
-           str_replace_all(
-             donor_name,
-             "Adp",
-             "ADP"),
-         donor_name=
-           str_replace_all(
-             donor_name,
-             "Alamance County Republican Party|Alamance County of Republican Party",
-             "Alamance County of Republican Party Building Fund"),
-         donor_name=
-           str_replace_all(
-             donor_name,
-             "Alamance County Republican Women's Club|Alamance Republican Women of NC",
-             "Alamance Republican Women"),
-         donor_name=
-           str_replace_all(
-             donor_name,
-             " Exec ",
-             " Executive "),
-         donor_name=
-           str_replace_all(
-             donor_name,
-             "Alexander Republican Party",
-             "Alexander County Republican Party"),
-         donor_name=
-           str_replace_all(
-             donor_name,
-             "Alleghany County Repbulican Part",
-             "Alleghany County Republican Party"),
-         donor_name=
-           str_replace_all(
-             donor_name,
-             "American Anestesiology of NC PAC|American Anethesiology of NC PAC",
-             "American Anesthesiology of NC PAC"),
-         donor_name=
-           str_replace_all(
-             donor_name,
-             "American Federation of State County and Municipal Employees AFSCME|American Fed of State Cty & Municipal Emp Afscme-People",
-             "American Federation of State County and Municipal Employees (AFSCME-PEOPLE)"),
-         donor_name=
-           str_replace_all(
-             donor_name,
-             "Anesthesiologist of the Triad PAC|Anesthessologist of the Triad PA",
-             "Anesthesiologists of the Triad PAC"),
-         donor_name=
-           str_replace_all(
-             donor_name,
-             "Anson County NC Republican Party",
-             "Anson County Republican Party"),
-         donor_name=
-           str_replace_all(
-             donor_name,
-             "Aristotle",
-             "Aristotle PAC"),
-         donor_name=
-           str_replace_all(
-             donor_name,
-             "Asd",
-             "ASD"),
-         donor_name=
-           str_replace_all(
-             donor_name,
-             "Gop",
-             "GOP"),
-         donor_name=
-           str_replace_all(
-             donor_name,
-             "Atmc",
-             "ATMC"),
-         donor_name=
-           str_replace_all(
-             donor_name,
-             "NC Auto Dealers Assn PAC|Autopac",
-             "NC Auto Dealers Accociation PAC"),
-         donor_name=
-           str_replace_all(
-             donor_name,
-             " (Iatse 491)",
-             ""),
-      )
 
 
 
 
 #don't forget row 310 w AT&T
 
-entity_donors %>%
-  distinct(donor_name) %>%
-  count()
-#1605
 
-autopac
